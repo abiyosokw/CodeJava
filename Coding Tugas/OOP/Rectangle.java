7139f49da006f0f3771a56a97815bdd6d924a1aa
Abiyoso Wibowo <abiyosowibowo@gmail.com>
2025-10-27 14:09:27 +0700

    Rectangle.java

---
+++ Coding Tugas/OOP/Rectangle.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class Rectangle {
+    double width;
+    double height;
+
+    public Rectangle() {
+        width = 1;
+        height = 1;
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
